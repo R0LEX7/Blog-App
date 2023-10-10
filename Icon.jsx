b43c1bf5f0e4321b7550eaf06d42fab4726a1cf8
R0LEX& <himanshugola1111@gmail.com>
2023-10-10 17:40:23 +0530

    Title and logo updated

---
+++ Icon.jsx
@@ -0,0 +1,11 @@
+import { FaMastodon } from 'react-icons/fa';
+
+import React from 'react'
+
+const Icon = () => {
+  return (
+    <FaMastodon className='App-Logo' />
+  )
+}
+
+export default Icon
