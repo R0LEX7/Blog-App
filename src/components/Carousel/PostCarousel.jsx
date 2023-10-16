a604f47e1327fb9d0a3954bdd58fa7df98f5a2fa
R0LEX& <himanshugola1111@gmail.com>
2023-10-17 00:16:14 +0530

    add vercel.json

--- src/components/Carousel/PostCarousel.jsx
+++ src/components/Carousel/PostCarousel.jsx
@@ -36,7 +36,7 @@ const PostCarousel = ({posts}) => {
         {posts && posts.map((post) => (
           <div
             key={post.$id}
-            className="flex items-center justify-center p-0   mt-16 lg:mt-0 md:mt-0 mr-4 gap-4"
+            className=" w-[300px] flex items-center justify-center p-0   mt-16 lg:mt-0 md:mt-0 gap-4"
           >
             <PostCard {...post} />
           </div>
